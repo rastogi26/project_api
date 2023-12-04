9b063c7bc9b4ad6e88915b60dd2313feda3cf572
dhruv <dr26dhruv@gmail.com>
2023-12-04 21:48:26 +0530

    updated model

--- models/model.go
+++ models/model.go
@@ -6,5 +6,5 @@ type Netflix struct {
      ID      string `json:"_id,omitempty" bson:"_id,omitempty"`
 	 Title   string `json:"title" bson:"title"`
 	 Genre   string `json:"genre" bson:"genre"`
-
+     Watched bool   `json:"watched" bson:"watched"`
 }
