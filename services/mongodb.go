074405be244715b6f7f855eb9bede6352d9c2996
dhruv <dr26dhruv@gmail.com>
2023-12-06 23:06:37 +0530

    database

--- services/mongodb.go
+++ services/mongodb.go
@@ -1,6 +1,10 @@
 package services
 
 import (
+	"context"
+	"fmt"
+	"log"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -17,4 +21,19 @@ func init() {
 	connectionString := "mongodb://localhost:27017"
 	clientOptions := options.Client().ApplyURI(connectionString)
 
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+    fmt.Println("MongoDB client created successfully")
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+    fmt.Println("Ping to MongoDB successful")
+	fmt.Println("Connected to MongoDB!")
+
+	Client = client
+	fmt.Printf("Client: %v\n", Client)
 }
+
