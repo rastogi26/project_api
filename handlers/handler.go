205efc14a6d03f27014762f43fb2d9aca0aac8bc
dhruv <dr26dhruv@gmail.com>
2023-12-11 20:07:57 +0530

    addMovie and GetAllMovies handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,2 +1,29 @@
 package handlers
 
+import(
+	"github.com/rastogi26/gofr-netflix/models"
+	"github.com/rastogi26/gofr-netflix/services"
+	"gofr.dev/pkg/gofr"
+)
+
+type NetflixHandler struct{}
+
+func NewNetflixHandler() NetflixHandler {
+	return NetflixHandler{}
+}
+
+func (h NetflixHandler) GetMyAllMovies(ctx *gofr.Context) (interface{}, error) {
+    movies := services.GetAllMovies()
+	return movies, nil
+}
+
+
+func (h NetflixHandler) AddMovie(ctx *gofr.Context) (interface{}, error) {
+	var movie models.Netflix
+	err := ctx.Bind(&movie)
+	if err != nil {
+		return nil, err
+	}
+    services.InsertOneMovie(movie)
+	return "Movie added successfully", nil
+}
