ef21b832a9cee5e1bddb180ebc35cd07540542a2
dhruv <dr26dhruv@gmail.com>
2023-12-11 22:41:05 +0530

    watched,remove movie and removeAllmovie handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -4,6 +4,7 @@ import(
 	"github.com/rastogi26/gofr-netflix/models"
 	"github.com/rastogi26/gofr-netflix/services"
 	"gofr.dev/pkg/gofr"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type NetflixHandler struct{}
@@ -26,4 +27,29 @@ func (h NetflixHandler) AddMovie(ctx *gofr.Context) (interface{}, error) {
 	}
     services.InsertOneMovie(movie)
 	return "Movie added successfully", nil
+}
+
+func (h NetflixHandler) WatchedMovie(ctx *gofr.Context) (interface{}, error) {
+	movieId := ctx.Param("id")
+	services.UpdateOneMovie(movieId)
+	return "Movie marked as watched", nil
+}
+
+func (h NetflixHandler) RemoveAllMovies(ctx *gofr.Context) (interface{}, error) {
+	deleteCount := services.DeleteAllMovie()
+	return deleteCount, nil
+}
+
+
+func (h NetflixHandler) RemoveMovie(ctx *gofr.Context) (interface{}, error) {
+	movieId := ctx.Param("id")
+
+	// Check if movieId is a valid ObjectId
+    _, err := primitive.ObjectIDFromHex(movieId)
+
+	if err != nil {
+		return nil,err
+	}
+      services.DeleteOneMovie(movieId)
+	  return "Movie removed successfully",nil
 }
