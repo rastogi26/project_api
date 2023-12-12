ad4f6e2e7484b8fa8b85998818faa579807a8b1c
dhruv <dr26dhruv@gmail.com>
2023-12-12 18:02:12 +0530

    updated main.go

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-
-func main()  {
+import (
 	
+	"gofr.dev/pkg/gofr"
+	
+	"github.com/rastogi26/gofr-netflix/routers"
+)
+func main()  {
+	app := gofr.New()
+
+	// Bypass header validation during API calls
+	app.Server.ValidateHeaders = false
+
+	// Setup Netflix routes
+	routers.SetupNetflixRoutes(app)
+
+	// Start the application
+	app.Start()
+
 }
